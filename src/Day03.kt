141586a07f9e2c88ddde3bf5127767619157c571
Roman Golyshev <anstreth@gmail.com>
2022-12-03 12:37:56 +0100

    Day 3, Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -6,11 +6,10 @@ fun main() {
         else -> error("Unexpected item type '$itemType'")
     }
 
-    fun findCommonItem(left: String, right: String): Char {
-        val commonChars = left.toSet() intersect right.toSet()
+    fun findCommonItems(left: String, right: String): String {
+        val commonItems = left.toSet() intersect right.toSet()
 
-        return commonChars.singleOrNull()
-            ?: error("There are multiple common char between $left and $right")
+        return String(commonItems.toCharArray())
     }
 
     fun part1(input: List<String>): Int {
@@ -20,16 +19,35 @@ fun main() {
             val left = rucksack.substring(0, rucksack.length / 2)
             val right = rucksack.substring(rucksack.length / 2)
 
-            val commonItem = findCommonItem(left, right)
+            val commonItem = findCommonItems(left, right).singleOrNull()
+                ?: error("There are multiple common items between $left and $right")
 
             itemPriority(commonItem)
         }
     }
 
+    fun part2(input: List<String>): Int {
+        require(input.size % 3 == 0)
+
+        return input
+            .windowed(size = 3, step = 3)
+            .sumOf { group ->
+                val (one, two, three) = group
+                val commonItems = findCommonItems(findCommonItems(one, two), three)
+
+                val commonItem = commonItems.singleOrNull()
+                    ?: error("There are multiple common items in the group: $group")
+
+                itemPriority(commonItem)
+            }
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
+    println(part2(input))
 }
