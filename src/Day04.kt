9bf5c6c0ea607ffdb7b78870b77f8407eb1fe221
Roman Golyshev <anstreth@gmail.com>
2022-12-04 14:03:24 +0100

    Day 4, Part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,6 +6,14 @@ fun main() {
 
         fun contains(other: Range): Boolean =
             from <= other.from && other.to <= to
+
+        fun intersects(other: Range): Boolean =
+            when {
+                to < other.from -> false
+                other.to < from -> false
+
+                else -> true
+            }
     }
 
     fun parseRange(range: String): Range {
@@ -14,19 +22,35 @@ fun main() {
         return Range(from.toInt(), to.toInt())
     }
 
+    fun parsePairOrRanges(pair: String): Pair<Range, Range> {
+        val ranges = pair.split(",", limit = 2)
+        val (left, right) = ranges.map { parseRange(it) }
+
+        return left to right
+    }
+
     fun part1(input: List<String>): Int {
         return input.count { pairOfRanges ->
-            val ranges = pairOfRanges.split(",", limit = 2)
-            val (left, right) = ranges.map { parseRange(it) }
+            val (left, right) = parsePairOrRanges(pairOfRanges)
 
             left.contains(right) || right.contains(left)
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.count { pairOfRanges ->
+            val (left, right) = parsePairOrRanges(pairOfRanges)
+
+            left.intersects(right)
         }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
+    println(part2(input))
 }
