94d9f93b4e93eccf7bfafb5f1cbe69aba01a564d
Roman Golyshev <anstreth@gmail.com>
2022-12-06 14:14:57 +0100

    Day 6, Part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun consistsOfUniqueChars(s: String) = s.toSet().size == s.length
+    fun isMarker(s: String): Boolean {
+        val alreadySeen = mutableSetOf<Char>()
+
+        return s.all {
+            alreadySeen.add(it)
+        }
+    }
+
+    fun findPositionAfterMarker(message: String, markerLength: Int): Int {
+        val startOfMarker = message.windowedSequence(size = markerLength).indexOfFirst { isMarker(it) }
+        return markerLength + startOfMarker
+    }
 
     fun part1(input: List<String>): Int {
         val message = input.single()
 
-        return 4 + message.windowedSequence(4)
-            .indexOfFirst { consistsOfUniqueChars(it) }
+        return findPositionAfterMarker(message, 4)
+    }
+
+    fun part2(input: List<String>): Int {
+        val message = input.single()
+
+        return findPositionAfterMarker(message, 14)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 19)
 
     val input = readInput("Day06")
     println(part1(input))
+    println(part2(input))
 }
