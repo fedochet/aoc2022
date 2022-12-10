16e57434f59cd40745c8de86e8016c17cc22cf51
Roman Golyshev <anstreth@gmail.com>
2022-12-10 01:03:48 +0100

    Day 9, Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -59,10 +59,9 @@ fun main() {
         return Move(direction, stepsStr.toInt())
     }
 
-    fun part1(input: List<String>): Int {
-        val snakeGame = SnakeGame(cells = 2)
+    fun countTailLength(snakeLength: Int, moves: List<Move>): Int {
+        val snakeGame = SnakeGame(cells = snakeLength)
 
-        val moves = input.map { parseMove(it) }
         for (move in moves) {
             repeat(move.steps) {
                 snakeGame.move(move.direction)
@@ -72,10 +71,22 @@ fun main() {
         return snakeGame.visitedCells
     }
 
+    fun part1(input: List<String>): Int {
+        val moves = input.map { parseMove(it) }
+        return countTailLength(2, moves)
+    }
+
+    fun part2(input: List<String>): Int {
+        val moves = input.map { parseMove(it) }
+        return countTailLength(10, moves)
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 13)
+    check(part1(testInput) == 88)
+    check(part2(testInput) == 36)
 
     val input = readInput("Day09")
     println(part1(input))
+    println(part2(input))
 }
