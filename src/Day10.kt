b254a2cd3b91250a32e4a67be8e44530cfb2ed5d
Roman Golyshev <anstreth@gmail.com>
2022-12-11 04:58:40 +0100

    Day 10, Part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -18,7 +18,7 @@ fun main() {
         }
     }
 
-    fun part1(input: List<String>): Int {
+    fun parseAndConvertToAssembly(input: List<String>): List<Assembly> {
         val initialCommands = input.map { parseCommand(it) }
         val assembly = initialCommands
             .flatMap {
@@ -28,20 +28,20 @@ fun main() {
                 }
             }
 
-        val recordedCycles =
-            listOf(20, 60, 100, 140, 180, 220)
-                .associateWith { 0 }
-                .toMutableMap()
+        return assembly
+    }
 
+    class Executor(val assembly: List<Assembly>) {
         var register = 1
+            private set
+
         var cycle = 1
+            private set
 
-        for (cmd in assembly) {
-            if (cycle in recordedCycles) {
-                recordedCycles[cycle] = register
-            }
+        fun nextStep() {
+            require(hasNext)
 
-            when (cmd) {
+            when (val cmd = assembly[cycle - 1]) {
                 is Add -> register += cmd.value
                 Noop -> {}
             }
@@ -49,13 +49,74 @@ fun main() {
             cycle += 1
         }
 
+        val hasNext: Boolean
+            get() = cycle < assembly.size
+    }
+
+
+    fun part1(input: List<String>): Int {
+        val assembly = parseAndConvertToAssembly(input)
+
+        val recordedCycles =
+            listOf(20, 60, 100, 140, 180, 220)
+                .associateWith { 0 }
+                .toMutableMap()
+
+        val executor = Executor(assembly)
+
+        while (executor.hasNext) {
+            if (executor.cycle in recordedCycles) {
+                recordedCycles[executor.cycle] = executor.register
+            }
+
+            executor.nextStep()
+        }
+
         return recordedCycles.toList().sumOf { (cycles, value) -> cycles * value }
     }
+
+    fun part2(input: List<String>): String {
+        val assembly = parseAndConvertToAssembly(input)
+        val executor = Executor(assembly)
+
+        val width = 40
+        val height = 6
+
+        return (0 until height).joinToString(separator = System.lineSeparator()) { _ ->
+            (0 until width).joinToString("") { widthPos ->
+                val sprite = executor.register
+
+                val pixelColor: String =
+                    if (widthPos in (sprite - 1)..(sprite + 1)) {
+                        "#"
+                    } else {
+                        "."
+                    }
+
+                executor.nextStep()
+
+                pixelColor
+            }
+        }
+    }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
     check(part1(testInput) == 13140)
+    val part2 = part2(testInput)
+    check(part2 == """
+            ##..##..##..##..##..##..##..##..##..##..
+            ###...###...###...###...###...###...###.
+            ####....####....####....####....####....
+            #####.....#####.....#####.....#####.....
+            ######......######......######......####
+            #######.......#######.......#######.....
+        """.trimIndent()
+    )
 
     val input = readInput("Day10")
     println(part1(input))
+    println(part2(input))
 }
+
