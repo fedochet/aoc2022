72ef78e98a4314fbcab6e0ffeeb45c1e2806f27d
Roman Golyshev <anstreth@gmail.com>
2022-12-12 17:34:11 +0100

    Day 12, Part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -12,10 +12,12 @@ fun main() {
         fun moveDown() = Pos(row - 1, column)
     }
 
+    operator fun <E> List<List<E>>.get(pos: Pos): E = this[pos.row][pos.column]
+
     data class ParseGraphResult(
         val graph: List<List<Int>>,
-        val from: Pos,
-        val to: Pos
+        val start: Pos,
+        val finish: Pos
     )
 
     fun parseGraph(input: List<String>): ParseGraphResult {
@@ -57,13 +59,16 @@ fun main() {
 
     data class PosAndDistance(val pos: Pos, val distance: Int)
 
-    fun part1(input: List<String>): Int {
-        val (graph, from, to) = parseGraph(input)
 
+    fun exploreGraph(
+        graph: List<List<Int>>,
+        start: Pos,
+        canGo: (from: Int, to: Int) -> Boolean
+    ): Sequence<PosAndDistance> = sequence {
         val rows = graph.size
         val columns = graph.first().size
 
-        val queue = ArrayDeque(listOf(PosAndDistance(from, 0)))
+        val queue = ArrayDeque(listOf(PosAndDistance(start, 0)))
         val visited = mutableSetOf<Pos>()
 
         while (queue.isNotEmpty()) {
@@ -73,28 +78,43 @@ fun main() {
                 continue
             }
 
-            if (current == to) {
-                return distance
-            }
+            yield(PosAndDistance(current, distance))
 
-            val currentHeight = graph[current.row][current.column]
+            val currentHeight = graph[current]
 
             val nextPositions =
                 sequenceOf(current.moveLeft(), current.moveRight(), current.moveUp(), current.moveDown())
                     .filter { it.row in 0 until rows && it.column in 0 until columns }
-                    .filter { graph[it.row][it.column] <= currentHeight + 1 }
+                    .filter { canGo(currentHeight, graph[it]) }
                     .filter { it !in visited }
 
             queue += nextPositions.map { PosAndDistance(it, distance + 1) }
         }
+    }
 
-        error("We should have visited '$to' position")
+    fun part1(input: List<String>): Int {
+        val (graph, start, finish) = parseGraph(input)
+
+        return exploreGraph(graph, start, canGo = { from, to -> to <= from + 1 })
+            .first { it.pos == finish }
+            .distance
+    }
+
+    fun part2(input: List<String>): Int {
+        val (graph, _, finish) = parseGraph(input)
+
+        return exploreGraph(graph, start = finish, canGo = { from, to -> from <= to + 1 })
+            .filter { graph[it.pos] == 0 }
+            .minBy { it.distance }
+            .distance
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
     check(part1(testInput) == 31)
+    check(part2(testInput) == 29)
 
     val input = readInput("Day12")
     println(part1(input))
+    println(part2(input))
 }
